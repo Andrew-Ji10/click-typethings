ba382e70111bdde27971eff1f63a1dc579a637d3
Andrew Ji <jiandrew10@gmail.com>
2023-01-10 17:08:30 -0800

    now it looks up hello

--- clickType.py
+++ clickType.py
@@ -1,1 +1,12 @@
+import mouse, time, keyboard
 print("hello world")
+
+time.sleep(3)
+
+print(mouse.get_position())
+
+mouse.release()
+mouse.move(511, 62)
+mouse.click()
+keyboard.write("hello")
+keyboard.press('enter')
