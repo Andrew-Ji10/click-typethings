7737e7dea5fe9e8e37fa1ab4f2ba77d93b0cd371
Andrew Ji <jiandrew10@gmail.com>
2023-01-11 09:32:36 -0800

    added new tab function

--- clickType.py
+++ clickType.py
@@ -3,10 +3,26 @@ print("hello world")
 
 time.sleep(3)
 
-print(mouse.get_position())
+#mouse.release()
+#mouse.move(511, 62)
+#mouse.click()
+#keyboard.write("hello")
+#keyboard.press('enter')
 
-mouse.release()
-mouse.move(511, 62)
-mouse.click()
-keyboard.write("hello")
-keyboard.press('enter')
+def goToWebsite(website):
+    mouse.release()
+    mouse.move(511, 62)
+    mouse.click()
+    keyboard.write(website)
+    time.sleep(3)
+    keyboard.press('enter')
+
+def newTab():
+    mouse.release()
+    mouse.move(793, 170)
+    mouse.click()
+    print("here")
+    time.sleep(2)
+    keyboard.press_and_release('ctrl+t')
+
+newTab()
